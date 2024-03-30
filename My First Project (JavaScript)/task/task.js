06c643ac70b12a35be986109a41a1a250d566d96
Jennifer Nordell <subdev338@gmail.com>
2024-03-30 09:43:18 -0400

    simple console.log to pass the challenges set forth

--- My First Project (JavaScript)/task/task.js
+++ My First Project (JavaScript)/task/task.js
@@ -7,4 +7,10 @@ const input = require('sync-input');
 // let age = Number(input("Type your age: "));
 // You will need it in later stages.
 
-console.log("Hello World!");
+console.log("Prices:");
+console.log("Bubblegum: $2");
+console.log("Toffee: $0.2");
+console.log("Ice cream: $5");
+console.log("Milk chocolate: $4");
+console.log("Doughnut: $2.5");
+console.log("Pancake: $3.2");
