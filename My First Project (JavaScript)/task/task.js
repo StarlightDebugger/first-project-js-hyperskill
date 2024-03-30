86997b292bd159fb2fa6e1970ce26336a9480aa9
Jennifer Nordell <subdev338@gmail.com>
2024-03-29 23:31:03 -0400

    initial commit

---
+++ My First Project (JavaScript)/task/task.js
@@ -0,0 +1,10 @@
+const input = require('sync-input');
+
+// We have imported the 'sync-input' package for you.
+// This package allows you to get user input.
+// Like so:
+// let name = input("Type your name: ");
+// let age = Number(input("Type your age: "));
+// You will need it in later stages.
+
+console.log("Hello World!");
