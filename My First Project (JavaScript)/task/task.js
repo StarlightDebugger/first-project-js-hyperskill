7e77018fd8bfeff85f38d967f0673674699eaedc
Jennifer Nordell <subdev338@gmail.com>
2024-04-01 14:38:13 -0400

    simple print statements and looping through an object to print and calculate sum

--- My First Project (JavaScript)/task/task.js
+++ My First Project (JavaScript)/task/task.js
@@ -6,11 +6,28 @@ const input = require('sync-input');
 // let name = input("Type your name: ");
 // let age = Number(input("Type your age: "));
 // You will need it in later stages.
+const inventory = {
+    "Bubblegum": 202,
+    "Toffee": 118,
+    "Ice cream": 2250,
+    "Milk chocolate": 1680,
+    "Doughnut": 1075,
+    "Pancake": 80,
+}
 
-console.log("Prices:");
-console.log("Bubblegum: $2");
-console.log("Toffee: $0.2");
-console.log("Ice cream: $5");
-console.log("Milk chocolate: $4");
-console.log("Doughnut: $2.5");
-console.log("Pancake: $3.2");
+function printInventory() {
+    console.log("Earned amount:")
+    for(const [item, price] of Object.entries(inventory)){
+        console.log(`${item}: $${price}`);
+    }
+    console.log(`Income: $${getTotal()}`);
+}
+
+function getTotal() {
+    let sum = 0;
+    for(const price of Object.values(inventory)) {
+        sum += price;
+    }
+    return sum;
+}
+printInventory();
